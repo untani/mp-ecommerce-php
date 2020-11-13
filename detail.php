f154a23301233dd4171cd104d32dfe1fcbcde88c
untani <e.mendozaind@gmail.com>
2020-11-13 15:39:15 -0600

    SDk Integration

--- detail.php
+++ detail.php
@@ -149,7 +149,6 @@ $preference->save();
                                             <?php echo "$" . $_POST['unit'] ?>
                                         </h3>
                                     </div>
-                                    <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button>
                                     <script src="https://www.mercadopago.com.mx/integrations/v1/web-payment-checkout.js" data-preference-id="<?php echo $preference->id; ?>"> </script>
                                 </div>
                             </div>
