959f75c4553c1ada78f1759ea54d7f49cdc20d6c
untani <e.mendozaind@gmail.com>
2020-11-26 16:35:42 -0600

    Exam

--- detail.php
+++ detail.php
@@ -1,7 +1,6 @@
 <?php
 // SDK de Mercado Pago
 require __DIR__ .  '/libs/vendor/autoload.php';
-ia
 // Credenciales
 MercadoPago\SDK::setAccessToken('APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181');
 MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
