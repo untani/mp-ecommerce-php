b73d9a11f329252a63b66160486fabeb164035af
untani <e.mendozaind@gmail.com>
2020-11-26 16:50:38 -0600

    Exam

--- detail.php
+++ detail.php
@@ -5,6 +5,63 @@ require __DIR__ .  '/libs/vendor/autoload.php';
 MercadoPago\SDK::setAccessToken('APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181');
 MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
+// Objeto de Prferencia
+$preference = new MercadoPago\Preference();
+
+// Agregamos el limite de cuotas y excluimos métodos de pago
+$preference->payment_methods = array(
+  "excluded_payment_methods" => array(
+    array("id" => "amex")
+  ),
+  "excluded_payment_types" => array(
+    array("id" => "atm")
+  ),
+  "installments" => 6
+);
+
+// Agregamos la información del payer
+  $payer = new MercadoPago\Payer();
+  $payer->name = "Lalo";
+  $payer->surname = "Landa";
+  $payer->email = "test_user_81131286@testuser.com";
+  $payer->phone = array(
+    "area_code" => "52",
+    "number" => "5549737300"
+  );
+  
+  $payer->address = array(
+    "street_name" => "Insurgentes Sur",
+    "street_number" => 1602,
+    "zip_code" => "03940"
+  );
+
+// Agregamos la información del producto
+    $item = new MercadoPago\Item();
+    $item->id = "1234";
+    $item->title = $_POST['title'];
+    $item->description = 'Dispositivo móvil de Tienda e-commerce';
+    $item->picture_url = $_POST['img'];
+    $item->quantity = 1;
+    $item->curency_id = "MXN";
+    $item->unit_price = $_POST['price'];
+
+// Agregamos la referencia externa
+    $preference->external_reference = "e.mendozaind@gmail.com";
+
+
+// Agregamos los retornos de dirección
+    $preference->back_urls = array(
+        "success" => "success.php",
+        "failure" => "failure.php",
+        "pending" => "pending.php"
+    );
+
+
+// Agregamos el auto-retorno
+    $preference->auto_return = "approved";
+
+
+
 
 
 ?>
